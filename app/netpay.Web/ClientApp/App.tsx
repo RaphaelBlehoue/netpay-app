07993f6cf25e0e26d355f70ac0798719b0211b0b
SAMPAN Blehoue Hermann Raphael <raphaelsampan@gmail.com>
2025-05-29 01:46:37 +0200

    chore(config) [us002] add splash screen and lottie animation

--- app/netpay.Web/ClientApp/App.tsx
+++ app/netpay.Web/ClientApp/App.tsx
@@ -1,10 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect, useState } from "react";
+import SplashScreenToolkit from "@/components/SplashScreen/SplashScreen";
 
+SplashScreen.preventAutoHideAsync();
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+  const [fontsLoaded, fontError] = useFonts({
+    "Inter-Black": require("./src/assets/fonts/Inter-Black.ttf"),
+    "Inter-Bold": require("./src/assets/fonts/Inter-Bold.ttf"),
+    "Inter-Medium": require("./src/assets/fonts/Inter-Medium.ttf"),
+    "Inter-Regular": require("./src/assets/fonts/Inter-Regular.ttf"),
+    "Inter-SemiBold": require("./src/assets/fonts/Inter-SemiBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady || (!fontsLoaded && !fontError)) {
+    return <SplashScreenToolkit onAnimationFinish={onLayoutRootView} />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={styles.text}>Welcome your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +56,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontFamily: "Inter-SemiBold",
+    fontSize: 32,
   },
 });
--- app/netpay.Web/ClientApp/src/components/SplashScreen/SplashScreen.tsx
+++ app/netpay.Web/ClientApp/src/components/SplashScreen/SplashScreen.tsx
@@ -1,12 +1,40 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { StyleSheet } from "react-native";
+import React, { useRef } from "react";
+import LottieView from "lottie-react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { COLORS } from "@/constants/Colors";
 
-const SplashScreen = () => {
+type Props = {
+  onAnimationFinish: () => void;
+};
+
+const SplashScreenToolkit: React.FC<Props> = ({ onAnimationFinish }) => {
+  const animation = useRef<LottieView>(null);
+
   return (
-    <View>
-      <Text>SplashScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={styles.animationContainer}>
+      <LottieView
+        autoPlay
+        loop={false}
+        ref={animation}
+        style={styles.splash}
+        source={require("../../assets/splash-screen.json")}
+        onAnimationFinish={onAnimationFinish}
+      />
+    </SafeAreaView>
+  );
+};
 
-export default SplashScreen
+const styles = StyleSheet.create({
+  animationContainer: {
+    flex: 1,
+    backgroundColor: COLORS.primary,
+  },
+  splash: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: COLORS.primary,
+  },
+});
+
+export default SplashScreenToolkit;
