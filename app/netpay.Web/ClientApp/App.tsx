8b3d4ffd3e332fec7d343cee76bd83058dc8d92f
SAMPAN Blehoue Hermann Raphael <raphaelsampan@gmail.com>
2025-05-29 02:53:22 +0200

    feat(navigation): [us-002] Add navigation for onbording page

--- app/netpay.Web/ClientApp/App.tsx
+++ app/netpay.Web/ClientApp/App.tsx
@@ -1,67 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { useFonts } from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
-import { useCallback, useEffect, useState } from "react";
-import SplashScreenToolkit from "@/components/SplashScreen/SplashScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Onboarding from "@/screens/Onboarding";
 
-SplashScreen.preventAutoHideAsync();
+const Stack = createNativeStackNavigator();
+
 export default function App() {
-  const [appIsReady, setAppIsReady] = useState(false);
-  const [fontsLoaded, fontError] = useFonts({
-    "Inter-Black": require("./src/assets/fonts/Inter-Black.ttf"),
-    "Inter-Bold": require("./src/assets/fonts/Inter-Bold.ttf"),
-    "Inter-Medium": require("./src/assets/fonts/Inter-Medium.ttf"),
-    "Inter-Regular": require("./src/assets/fonts/Inter-Regular.ttf"),
-    "Inter-SemiBold": require("./src/assets/fonts/Inter-SemiBold.ttf"),
-  });
-
-  useEffect(() => {
-    if (fontsLoaded || fontError) {
-      SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded, fontError]);
-
-  useEffect(() => {
-    async function prepare() {
-      try {
-        await new Promise((resolve) => setTimeout(resolve, 5000));
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        setAppIsReady(true);
-      }
-    }
-    prepare();
-  }, []);
-
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      SplashScreen.hideAsync();
-    }
-  }, [appIsReady]);
-
-  if (!appIsReady || (!fontsLoaded && !fontError)) {
-    return <SplashScreenToolkit onAnimationFinish={onLayoutRootView} />;
-  }
-
   return (
-    <View style={styles.container} onLayout={onLayoutRootView}>
-      <Text style={styles.text}>Welcome your app!</Text>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Onboarding}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontFamily: "Inter-SemiBold",
-    fontSize: 32,
-  },
-});
